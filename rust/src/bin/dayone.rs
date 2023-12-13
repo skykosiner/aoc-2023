6635be527dee10592ad7e3af51f29db65b55e821
Sky Kosiner <sky@skykosiner.com>
2023-12-13 16:45:46 +0000

    Rust day one part two

--- rust/src/bin/dayone.rs
+++ rust/src/bin/dayone.rs
@@ -19,10 +19,6 @@ fn part_one(lines: std::str::Split<'_, char>) {
     let mut total = 0;
 
     for line in lines {
-        if line.trim().is_empty() {
-            continue;
-        }
-
         let points: Vec<i32> = line
             .chars()
             .filter_map(|x| x.to_digit(10))
@@ -39,7 +35,40 @@ fn part_one(lines: std::str::Split<'_, char>) {
     println!("Total for part one {}", total);
 }
 
+fn part_two(lines: std::str::Split<'_, char>) {
+    // let i = 0;
+    let mut total = 0;
+
+    for line in lines {
+        if line.is_empty() {
+            continue;
+        }
+
+        let mut digits = Vec::new();
+
+        for (i, c) in line.chars().enumerate() {
+            if c.is_digit(10) {
+                digits.push(c.to_digit(10).unwrap() as i32);
+            }
+
+            let prefixes = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+            for (d, val) in prefixes.iter().enumerate() {
+                if line.get(i..).unwrap_or("").starts_with(val) {
+                    digits.push((d + 1) as i32);
+                }
+            }
+        }
+
+        if !digits.is_empty() {
+            total += combine_first_and_last(digits);
+        }
+    }
+
+    println!("Total for day one part two {}", total);
+}
+
 fn main() {
-    // Part one
-    part_one(fs::read_to_string("./src/bin/day1.test").expect("File not found").split('\n'));
+    part_one(fs::read_to_string("./src/bin/day1.input").expect("File not found").split('\n'));
+    part_two(fs::read_to_string("./src/bin/day1.input").expect("File not found").split('\n'));
 }
